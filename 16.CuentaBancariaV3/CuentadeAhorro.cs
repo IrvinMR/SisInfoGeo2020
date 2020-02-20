77ec8b94e947974cc24de456f109da6b47913e77
Irvin Medina <irvin.medina15@gmail.com>
2020-02-20 11:40:50 -0600

    subido

---
+++ 16.CuentaBancariaV3/CuentadeAhorro.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _14.CuentaBancaria{
+    class CuentadeAhorro : CuentaBancaria{ //heredamos de cuenta bancaria
+    private double tasaInteres;
+
+    public CuentadeAhorro(double saldo, double tasaInteres)
+        : base(saldo) { //llamada explicita al constructor clase base
+            this.tasaInteres=tasaInteres;
+        }
+
+        public void CalcularInteres(){
+            saldo+=(saldo*tasaInteres);
+        }
+    }
+}
