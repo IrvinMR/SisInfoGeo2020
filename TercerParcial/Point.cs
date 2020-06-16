a104b25debae044d7efecb5d93b50b5a0e452a6d
Irvin Medina <irvin.medina15@gmail.com>
2020-06-16 00:57:38 -0500

    Este es el bueno

--- TercerParcial/Point.cs
+++ TercerParcial/Point.cs
@@ -3,14 +3,13 @@ using System;
 namespace TercerParcial{
     class Program{
 
-public struct Point
-{
-    public double X { get; set; }
-    public double Y { get; set; }
-    public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+    public struct Point{
+        public double X { get; set; }
+        public double Y { get; set; }
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
 
-    public readonly override string ToString() =>
-        $"({X}, {Y}) is {Distance} from the origin";
-}
-}
+        public readonly override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+        }
+    }
 }
